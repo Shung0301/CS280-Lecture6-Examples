651c7c88abb49fff85093b7d2b942923af57fb83
Shung0301 <tylertangbu@gmail.com>
2017-03-21 20:05:59 +0800

    First Update

---
+++ PokemonLibrary/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public int NationalNo { get; set; }
+        public string[] Type { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+    }
+}
